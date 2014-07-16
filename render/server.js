7cb431eda012dd9ac13b87b90bc55a808caf9d93
James Halliday <mail@substack.net>
2014-07-16 16:25:22 -0700

    use data attrs

--- render/server.js
+++ render/server.js
@@ -5,8 +5,8 @@ var html = fs.readFileSync(__dirname + '/server.html', 'utf8');
 module.exports = function () {
     return hyperspace(html, function (row) {
         return {
-            '.name .value': row.name,
-            '.address .value': row.address
+            '*[data-name]': row.value.name,
+            '*[data-address]': row.value.address
         };
     });
 };
