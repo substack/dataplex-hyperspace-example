09f8e69c4444bd572143549b8bff469256b22357
James Halliday <mail@substack.net>
2014-07-16 17:50:34 -0700

    using hyperspace browser-side

--- browser/main.js
+++ browser/main.js
@@ -1,16 +1,20 @@
 var through = require('through2');
 var Graph = require('./graph.js');
+var path = require('path');
 
 var sock = require('shoe')('/sock');
 var plex = require('dataplex')();
 sock.pipe(plex).pipe(sock);
 
+var bulk = require('bulk-require');
+var render = bulk(__dirname + '/../render', [ '*.js' ]);
+
 var graphs = [];
 
-var servers = [].slice.call(document.querySelectorAll('.server'));
-servers.forEach(function (server) {
-    var name = server.querySelector('.name').textContent;
-    var graph = Graph().appendTo(server.querySelector('.graph'));
+var r = render.server().appendTo('#servers');
+r.on('element', function (elem) {
+    var name = elem.querySelector('.name').textContent;
+    var graph = Graph().appendTo(elem.querySelector('.graph'));
     plex.open('/server/' + name + '/cpu').pipe(graph);
     graphs.push(graph);
 });
